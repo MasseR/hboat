413283b458e569a4956275d94ccb06478bb56a1a
Mats Rauhala <mats.rauhala@iki.fi>
2019-03-06 22:39:53 +0200

    More prelude stuff

--- common/src/CustomPrelude.hs
+++ common/src/CustomPrelude.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module CustomPrelude
   ( module Prelude
+  , module System.Directory
+  , MonadIO(..)
   , ByteString
   , LByteString
   , Text
@@ -9,16 +11,24 @@ module CustomPrelude
   , listToMaybe
   , foldl'
   , scanl'
+  , tshow
   )
   where
 
-import Prelude hiding (scanl, foldl)
-import qualified Data.ByteString as B
+import           Control.Monad.Trans  (MonadIO (..))
+import qualified Data.ByteString      as B
 import qualified Data.ByteString.Lazy as LB
-import Data.Text (Text)
-import GHC.Generics (Generic)
-import Data.Maybe (fromMaybe, listToMaybe)
-import Data.List (foldl', scanl')
+import           Data.List            (foldl', scanl')
+import           Data.Maybe           (fromMaybe, listToMaybe)
+import           Data.Text            (Text)
+import           GHC.Generics         (Generic)
+import           Prelude              hiding (foldl, scanl)
+import           System.Directory     (doesFileExist, getDirectoryContents,
+                                       listDirectory)
+import qualified Data.Text as T
 
 type ByteString = B.ByteString
 type LByteString = LB.ByteString
+
+tshow :: Show a => a -> T.Text
+tshow = T.pack . show
